70eb76b9bcf026ceebf69a2b81bec75caf0df6c6
Shaina0101 <shainasaleem951@gmail.com>
2023-02-02 06:54:52 +0530

    Aiza-Works

--- src/org/base/Test.java
+++ src/org/base/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		public static void main(String[] args) {
 			String s = "Shaina";
 			System.out.println(s);
+			boolean empty = s.isEmpty();
+			System.out.println(empty);
 		}
 		
 	}
